cfb8b77b3987486dc7eeeb41de9b84acaafc65f1
Molka <molka.zaw@gmail.com>
2024-06-09 16:08:34 +0200

    feat: add a form to convert an array of numbers

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { MAX_NUMBER_CONVERTED, NumbersInFrench } from './NumbersInFrench';
 
 function App() {
+  const [input, setInput] = useState<string>("")
+  const [result, setResult] = useState<(string | undefined)[] | undefined>(undefined)
+  const [errorMessage, setErrorMessage] = useState<string | undefined>(undefined)
+
+  function handleInputChange(event: React.FormEvent<HTMLTextAreaElement>) {
+    const value = event.currentTarget.value
+    setInput(value)
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    // Clear error message
+    setErrorMessage(undefined)
+    try {
+      const parsedInput = JSON.parse(input)
+      if (!Array.isArray(parsedInput)) {
+        throw new Error("Input should be an array")
+      }
+      const res = parsedInput.map((n) => NumbersInFrench.convert(n))
+      setResult(res)
+    } catch (e) {
+      setResult(undefined)
+      setErrorMessage(`${e}`)
+    }
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: "30px", marginTop: "50px" }}>
+      <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column' }}>
+        <label>Enter an array of numbers you want to convert in French (numbers from 0 to {MAX_NUMBER_CONVERTED}):</label>
+        <textarea
+          value={input}
+          onChange={handleInputChange}
+          required
+        />
+        <button type="submit">Convert</button>
+      </form>
+      {errorMessage ? (<div style={{ color: 'red' }}>{errorMessage} </div>) : (<div>{JSON.stringify(result)}</div>)}
     </div>
   );
 }
